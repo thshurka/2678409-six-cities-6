a94e6f24be141214e7331d28a95e97a616841835
Alexander Gvozdev <thshurka@gmail.com>
2026-06-02 13:38:11 +0500

    Добавлены компоненты страниц и карточки предложения

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+const PLACES_COUNT = 312;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App placesCount={PLACES_COUNT} />
   </React.StrictMode>
 );
---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,28 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">404 Not Found</h1>
+            <a className="login__submit form__submit button" href="main.html">Go to the main page</a>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundPage;
